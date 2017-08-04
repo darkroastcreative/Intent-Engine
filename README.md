167564ce1d3697918f04c2a35e9a818aa2d756bd
David Fink <david@darkroastcreative.co>
2017-08-04 15:26:28 -0400

    Refactored intentEngine.js to use the ES6 class structure, and updates the README file to account for changes in the structure of the engine

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 The Intent Engine provides a framework for developers to easily determine and act on user intents within websites and apps.
 
 ## Installation
-To install the intent engine, simply include it in a directory where your website or application can access the file.
+To install the intent engine, simply include it in a directory where your website or application can access the file, and link the script in your site or application's files.
+
+## Dependencies
+* [jQuery](https://jquery.com/)
 
 ## Usage
 ### Setup
@@ -11,15 +14,13 @@ For the most basic setup, you can set the intent of a user by attaching the inte
 #### Example
 `<a href="http://yoursite.com/contact?intent=quote">Request A Quote</a>`
 
+To detect and act on intents, you will need an instance of Intent Engine. The recommended approach to this is creating a read-only instance using the `const` declaration, as demonstrated here:
+`const intentEngine = new IntentEngine();`
+
 This link will direct the user to the Contact page of this website, establishing that the user intends to request a quote, and allowing the site to respond to this specific intent.
 
 ### Detect Intent
-To detect the user’s intent, call `getIntent()`. It may be useful to store the return value as a variable.
-
-#### Example
-```
-var intent = getIntent();
-```
+To detect the user’s intent, call `getIntent()`. While the engine stores the intent value internally, it may be useful to store the return value as a variable.
 
 ### Act on the Intent
 Using the detected intent, determine what action your product should take. One of the simplest ways to do this is to use String comparisons in `if` statements, such as shown in the example below.
@@ -33,16 +34,19 @@ else if(intent.equals('intent2')	{
 	// Do something specific to intent 2
 }
 else	{
-	// Do nothing, or perform some defult action
+	// Do nothing, or perform some default action
 }
 ```
 
 ## Functions
 ### getIntent()
-`getIntent()` attempts to retrieve the intent of a user by checking for the `intent` query component in a URI, and if there is a value set for this component. If an intent is found, it will be returned as a String, otherwise the function will return `null`.
+`getIntent()` attempts to retrieve the intent of a user by checking for the `intent` query component in a URI, and if there is a value set for this component. The engine's internal `intent` variable will be set to the value (which may be null), and the internal `nullIntent` variable will be set to reflect whether or not the intent is null. In addition, the function will return the detected intent.
 
 ### getPages()
-For projects utilizing the Intent Engine page structure, the `getPages()` function checks for pages present in a given file, and returns a String array of the page names. If no pages are found, the function will return an empty array.
+For projects utilizing the Intent Engine page structure, the `getPages()` function checks for pages present in a given file, and sets the engine's internal `pages` variable to an array of found pages. If no pages are found, this variable will store an empty array.
 
 ### hasPage(pageName)
 For projects utilizing the Intent Engine page structure, `hasPage()` accepts a page name as a String, and returns a boolean value indicating whether or not the page exists.
+
+## showIntentContent(intent, transitionTime)
+For projects utilizing the Intent Engine page structure, `showIntentContent()` shows the page associated with the intent, and hides any other page that was visible to the user. The function accepts a String representing the current intent and a transition time for displaying the desired page (in milliseconds).
--- intentEngine.js
+++ intentEngine.js
@@ -6,63 +6,104 @@ david@darkroastcreative.co
 http://darkroastcreative.co
 */
 
-// Establish a set of variables for use with the engine
-var pages = [];
-var gotPages = false;
-var hasPages = false;
+class IntentEngine {
 
-function getIntent() {
+  constructor() {
+    // Establish a set of variables for use with the engine
+    var intent = this.getIntent();
+    var nullIntent = true;
+    var pages = this.getPages();
+    var gotPages = false;
+    var hasPages = false;
+  }
 
-  // Get query arguments from URI
-  var $_GET = {},
-    args = location.search.substr(1).split(/&/);
-  for (var i = 0; i < args.length; ++i) {
-    var tmp = args[i].split(/=/);
-    if (tmp[0] != "") {
-      $_GET[decodeURIComponent(tmp[0])] = decodeURIComponent(tmp.slice(1).join("").replace("+", " "));
+  getIntent() {
+
+    // Get query arguments from URI
+    var $_GET = {},
+      args = location.search.substr(1).split(/&/);
+    for (var i = 0; i < args.length; ++i) {
+      var tmp = args[i].split(/=/);
+      if (tmp[0] != "") {
+        $_GET[decodeURIComponent(tmp[0])] = decodeURIComponent(tmp.slice(1).join("").replace("+", " "));
+      }
     }
-  }
 
-  // Assign intent property (if it exists) to the intent variable
-  var intent = $_GET.intent;
-
-  // Return the detected intent (or lack thereof)
-  return intent;
-
-}
-
-function getPages() {
-
-  // Clear page name array
-  this.pages = [];
-
-  // Detect pages and add their names to the array
-  $.each($('.page'), function(i) {
-    pages.push($(this).attr('id'));
-  });
-
-  // Set gotPages to true
-  this.gotPages = true;
-
-  // If the array was populated with any pages, set hasPages to true
-  if(pages.length > 0)  {
-    this.hasPages = true;
-  }
-
-  // Return the array of page names
-  return this.pages;
-
-}
-
-function hasPage(pageName) {
-
-  // If the page list has already been retrieved, continue with checking for
-  // the specified page, otherwise get the page list and try again
-  if(this.gotPages)  {
-    if(this.hasPages && this.pages.indexOf(pageName) > -1)  {
-      return true;
+    // Assign intent property to the intent variable and set nullIntent variable accordingly
+    this.intent = $_GET.intent;
+    if (this.intent != null) {
+      this.nullIntent = false;
+      return this.intent;
     }
+    this.nullIntent = true;
+    return this.intent;
   }
-  return false;
+
+  getPages() {
+
+    // Clear page name array
+    var pages = [];
+
+    // Detect pages and add their names to the array
+    $.each($('.page'), function(i) {
+      pages.push($(this).attr('id'));
+    });
+
+    // Set gotPages to true
+    this.gotPages = true;
+
+    // If the array was populated with any pages, set hasPages to true
+    if (pages.length > 0) {
+      this.hasPages = true;
+    }
+
+    this.pages = pages;
+
+  }
+
+  hasPage(pageName) {
+
+    // If the page list has already been retrieved, continue with checking for
+    // the specified page, otherwise get the page list and try again
+    if (this.gotPages) {
+      if (this.hasPages && this.pages.indexOf(pageName) > -1) {
+        return true;
+      }
+    }
+    else {
+      getPages();
+      hasPage(pageName);
+    }
+    return false;
+
+  }
+
+  showIntentContent(transitionTime) {
+    // check for the presence of parameters in the function call and act accordingly
+    if (this.nullIntent) {
+      this.intent = this.getIntent();
+    }
+    console.log("Intent: " + this.intent);
+    if(!this.gotPages)
+    if (transitionTime == null) {
+      transitionTime = 350;
+    }
+
+    // Hide all pages
+    $.each($('.page'), function(i) {
+      $(this).hide();
+    });
+
+    // Show the content associated with the current intent
+    if(!this.nullIntent) {
+      if (this.hasPage(this.intent)) {
+        console.log("Has page");
+        $('#' + this.intent).show(transitionTime);
+        return;
+      }
+    }
+    $('#default').show(transitionTime);
+
+  }
 
 }
